40d9521a14df26a1856eee98e4a9af090f771273
SirDouglas <douglas.armore@gmail.com>
2019-05-27 21:04:39 -0300

    Create Exemplo001.java

---
+++ Exemplo001.java
@@ -0,0 +1,8 @@
+public class Exemplo001 {
+        public static void main(String[] args) {
+            System.out.println("O primeiro programa a gente nunca escreve");
+            System.out.print("Error ");
+            System.out.println("Ler");
+        }
+                
+}
