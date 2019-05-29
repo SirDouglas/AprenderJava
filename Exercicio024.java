b870dfc9d1673e511642736a939129e90bef9ce3
SirDouglas <douglas.armore@gmail.com>
2019-05-29 16:58:32 -0300

    Create Exercicio024.java

---
+++ Exercicio024.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+import java.math.*;
+public class Exercicio024 {
+
+	public static void main(String[] args) {
+		double numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
+	double numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite um segundo numero"));
+			System.out.println(Math.pow(numero1,numero2));
+			System.out.println(Math.sqrt(numero1));
+	}
+}
